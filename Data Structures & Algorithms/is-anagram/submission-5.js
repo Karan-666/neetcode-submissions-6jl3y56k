3a0d919abf1a91185a90396e67ee37e7a6f71e0e
Karan Kumar <68183807+Karan-666@users.noreply.github.com>
2026-04-02 22:37:12 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const m1 = new Map();
+
+        if(s.length !== t.length)
+            return false;
+
+        for(const i of s){
+            if(m1.has(i)){
+                m1.set(i, m1.get(i)+1)
+            }else{
+                m1.set(i, 1);
+            }
+        }
+
+
+        for (const i of t) {
+            if (!m1.has(i) || m1.get(i) === 0) {
+                return false;
+            }
+            m1.set(i, m1.get(i) - 1);
+        }
+
+        return true;
+    
+    }
+}
